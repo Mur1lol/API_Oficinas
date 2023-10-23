2867301318ff9298642ecdcadd477490bfff0532
Murilo <bc.murilo.mbc@gmail.com>
2023-10-23 01:21:42 -0300

    Adicioando pagina HTML

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-const express = require('express');
+import express = require('express');
+import cors = require('cors');
 import router from './router';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
